c8769bb07dd8ef4fec90515ab61a2a1d62fe18a6
mattleahy <matthew.m.leahy@outlook.com>
2020-01-13 13:03:05 +0000

    finished the seed file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -8,12 +8,27 @@ const {
 const { formatDates, formatComments, makeRefObj } = require("../utils/utils");
 
 exports.seed = function(knex) {
-  const topicsInsertions = knex("topics").insert(topicData);
-  const usersInsertions = knex("users").insert(userData);
-
-  return Promise.all([topicsInsertions, usersInsertions])
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
     .then(() => {
-      /* 
+      const topicsInsertions = knex("topics").insert(topicData);
+      const usersInsertions = knex("users").insert(userData);
+      return Promise.all([topicsInsertions, usersInsertions]);
+    })
+    .then(() => {
+      const formattedData = formatDates(articleData);
+      return knex("articles")
+        .insert(formattedData)
+        .returning("*");
+    })
+    .then(articleRows => {
+      const articleRef = makeRefObj(articleRows);
+      const formattedComments = formatComments(commentData, articleRef);
+      return knex("comments").insert(formattedComments);
+    });
+
+  /* 
       
       Your article data is currently in the incorrect format and will violate your SQL schema. 
       
@@ -21,9 +36,8 @@ exports.seed = function(knex) {
 
       Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
       */
-    })
-    .then(articleRows => {
-      /* 
+
+  /* 
 
       Your comment data is currently in the incorrect format and will violate your SQL schema. 
 
@@ -31,9 +45,4 @@ exports.seed = function(knex) {
       
       You will need to write and test the provided makeRefObj and formatComments utility functions to be able insert your comment data.
       */
-
-      const articleRef = makeRefObj(articleRows);
-      const formattedComments = formatComments(commentData, articleRef);
-      return knex("comments").insert(formattedComments);
-    });
 };
