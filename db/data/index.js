a574183a7f2593264c27cdde75fb31baa9a321d2
mattleahy <matthew.m.leahy@outlook.com>
2020-01-13 12:23:25 +0000

    set up migrations

---
+++ db/data/index.js
@@ -0,0 +1,11 @@
+const ENV = process.env.NODE_ENV || "development";
+
+const devData = require("./development-data/index.js");
+const testData = require("./test-data/index.js");
+
+const data = {
+  development: devData,
+  test: testData
+};
+
+module.exports = data[ENV];
---
+++ db/migrations/20200113091403_create_table_users.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("users", usersTable => {
+    usersTable
+      .string("username")
+      .primary()
+      .unique();
+    usersTable.string("avatar_url");
+    usersTable.string("name").notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("users");
+};
